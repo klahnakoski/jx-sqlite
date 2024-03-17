e24161e2bc401f8ebe265689845b79382f9c1538
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-03-17 08:39:08 -0400

    update version number

--- packaging/setup.py
+++ packaging/setup.py
@@ -16,5 +16,5 @@ setup(
     package_dir={},
     packages=["jx_sqlite","jx_sqlite.models","jx_sqlite.expressions"],
     url='https://github.com/klahnakoski/jx-sqlite',
-    version='5.570.24076'
+    version='5.571.24077'
 )
