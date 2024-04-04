09cd4db4e8200b3d1aaefe39d05149d37c79d213
Kyle Lahnakoski <kyle@lahnakoski.com>
2024-04-03 23:04:26 -0400

    updates from other projects

---
+++ tests/test_jx/test_joins.py
@@ -0,0 +1,35 @@
+# encoding: utf-8
+#
+#
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+#
+# Contact: Kyle Lahnakoski (kyle@lahnakoski.com)
+#
+from unittest import skip
+
+from mo_dots import list_to_data
+from mo_testing.fuzzytestcase import add_error_reporting
+from tests.test_jx import BaseTestCase, TEST_TABLE
+
+lots_of_data = list_to_data([{"a": i} for i in range(30)])
+
+
+@add_error_reporting
+@skip("not ready")
+class TestJoins(BaseTestCase):
+    def test_subtraction(self):
+        test = {
+            "data": [{"a": [{"v": 1}, {"v": 2}], "b": [{}, {"v": 1}]}],
+            "query": {
+                "select": {"name": ".", "value": "t"},
+                "from": [
+                    {"name": "t", "value": TEST_TABLE + ".a"},
+                    {"left_join": {"name": "u", "value": TEST_TABLE + ".a"}, "on": {"eq": ["t.v", "u.v"]}},
+                ],
+                "where": {"missing": "u"},
+            },
+            "expecting_list": {"meta": {"format": "list"}, "data": [{"v": 2}],},
+        }
+        self.utils.execute_tests(test)
